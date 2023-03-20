ca4d43517818b3a37284c72dfee69e27846a6379
Abel Pan <abel.pan9@gmail.com>
2023-03-20 01:18:00 -0700

    updated finder-test.sh for assignment4-part2

--- finder-app/finder-test.sh
+++ finder-app/finder-test.sh
@@ -8,7 +8,10 @@ set -u
 NUMFILES=10
 WRITESTR=AELD_IS_FUN
 WRITEDIR=/tmp/aeld-data
-username=$(cat conf/username.txt)
+CONFPATH=/etc/finder-app/conf
+RUNDIR=$(dirname $0)
+#CONFPATH=${RUNDIR}/../conf
+username=$(cat ${CONFPATH}/username.txt)
 
 if [ $# -lt 3 ]
 then
@@ -32,7 +35,7 @@ echo "Writing ${NUMFILES} files containing string ${WRITESTR} to ${WRITEDIR}"
 rm -rf "${WRITEDIR}"
 
 # create $WRITEDIR if not assignment1
-assignment=$(cat conf/assignment.txt)
+assignment=$(cat ${CONFPATH}/assignment.txt)
 echo $assignment
 echo ${username}
 echo $WRITESTR
@@ -52,15 +55,15 @@ then
 	fi
 fi
 #echo "Removing the old writer utility and compiling as a native application"
-make clean
-make
+# make clean
+# make
 
 for i in $( seq 1 $NUMFILES)
 do
-	./writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
+	${RUNDIR}/writer "$WRITEDIR/${username}$i.txt" "$WRITESTR"
 done
 
-OUTPUTSTRING=$(./finder.sh "$WRITEDIR" "$WRITESTR")
+OUTPUTSTRING=$(${RUNDIR}/finder.sh "$WRITEDIR" "$WRITESTR")
 
 # remove temporary directories
 rm -rf /tmp/aeld-data
